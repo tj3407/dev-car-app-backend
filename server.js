5acc44036afaa20d1086adf1ef2b9f5d29a7d7c6
TJ Sornet <33141343+tj3407@users.noreply.github.com>
2021-04-19 17:57:27 -0700

    Get vehicle data

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const client = new smartcar.AuthClient({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   redirectUri: process.env.REDIRECT_URI,
-  scope: ['required:read_vehicle_info', "read_odometer", "read_tires"],
+  scope: ['required:read_vehicle_info', "required:read_location", "required:read_tires", "required:read_odometer", "required:read_battery"],
   testMode: true,
 });
 
@@ -66,20 +66,45 @@ app.get('/vehicle', function(req, res) {
       return response;
     })
     .then(function(info) {
-        res.json(info);
+      const updatedObj = Object.assign({}, info);
+      updatedObj.refreshToken = access.refreshToken;
+      res.json(updatedObj);
     });
 });
 
-app.get('/odometer', function(req, res) {
+app.get('/getVehicleData', async function(req, res) {
   const id = req.query.vehicleId;
-  const url = `https://api.smartcar.com/v1.0/vehicles/${id}/odometer`;
+  const distanceUrl = `https://api.smartcar.com/v1.0/vehicles/${id}/odometer`;
+  const batteryLevel = `https://api.smartcar.com/v1.0/vehicles/${id}/battery`;
+  const tirePressure = `https://api.smartcar.com/v1.0/vehicles/${id}/tires/pressure`;
+  const location = `https://api.smartcar.com/v1.0/vehicles/${id}/location`;
+  const vehicleData = {};
 
-  return axios({
+  
+  vehicleData.distance = await axios({
     method: "GET",
-    url,
+    url: distanceUrl,
     headers: { 'Authorization': `Bearer ${access.accessToken}` }
   })
-    .then(info => res.json(info.data));
+    .then(info => info.data);
+  
+  vehicleData.batteryLevel = await axios({
+    method: "GET",
+    url: batteryLevel,
+    headers: { 'Authorization': `Bearer ${access.accessToken}` }
+  })
+    .then(info => info.data)
+    .catch(err => console.log('battery', err))
+
+  vehicleData.location = await axios({
+    method: "GET",
+    url: location,
+    headers: { 'Authorization': `Bearer ${access.accessToken}` }
+  })
+    .then(info => info.data)
+    .catch(err => console.log('tire pressure', err))
+
+  res.json(vehicleData);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
